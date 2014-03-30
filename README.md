dcf07d6f12b36010da57cfdefbcdf62f8e433688
Clément Prévost <prevostclement@gmail.com>
2014-03-30 23:40:48 +0200

    Add index_usage and database_size views

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ Compilation of function to administrate, migrate, postgres.
 
 ### Views
 
-* index_duplicates - List all index similar to each other, you should keep an eye on those indexes
+* index_duplicates - List all indexes similar to each other, you should keep an eye on those indexes
 * table_sizes - List all table sizes, index sizes and various size-related metrics
+* index_usage - List all indexes and index usage statistics, easily find unused indexes
+* database_size - List all databases and their disk usage
 
 ### Functions
 
--- postgres/extension/admin_utils--1.0.sql
+++ postgres/extension/admin_utils--1.0.sql
@@ -63,6 +63,7 @@ order by
 
 
 
+-- based on query from http://wiki.postgresql.org/wiki/Index_Maintenance
 CREATE VIEW index_duplicates AS 
     WITH index_duplicate AS (
         SELECT
@@ -102,4 +103,48 @@ CREATE VIEW index_duplicates AS
         duplicate_count > 1
     ORDER BY
         duplicate_id
+;
+
+
+
+
+-- based on query from http://wiki.postgresql.org/wiki/Index_Maintenance
+CREATE VIEW index_usage AS 
+    SELECT
+        pg_tables.schemaname as schema_name,
+        pg_tables.tablename as table_name,
+        index_pg_class.relname as index_name,
+        table_pg_class.reltuples AS num_rows,
+        pg_size_pretty(pg_relation_size(pg_tables.schemaname||'.'||pg_tables.tablename)) AS table_size,
+        pg_size_pretty(pg_relation_size(pg_tables.schemaname||'.'||index_pg_class.relname)) AS index_size,
+        CASE WHEN pg_index.indisunique THEN 'Y' ELSE 'N' END AS unique,
+        pg_stat_all_indexes.idx_scan AS number_of_scans,
+        pg_stat_all_indexes.idx_tup_read AS tuples_read,
+        pg_stat_all_indexes.idx_tup_fetch AS tuples_fetched
+    FROM pg_tables
+        LEFT JOIN pg_class table_pg_class ON pg_tables.tablename = table_pg_class.relname
+        LEFT JOIN pg_index ON table_pg_class.oid = pg_index.indrelid
+        LEFT JOIN pg_class as index_pg_class ON index_pg_class.oid = pg_index.indexrelid
+        LEFT JOIN pg_stat_all_indexes ON pg_index.indexrelid = pg_stat_all_indexes.indexrelid 
+    WHERE 
+        pg_tables.schemaname not in ('pg_catalog', 'information_schema')
+    ORDER BY 
+        schema_name, 
+        table_name
+;
+           
+
+-- based on query from http://wiki.postgresql.org/wiki/Disk_Usage
+CREATE VIEW database_size AS 
+    SELECT 
+        d.datname AS db_name,
+        pg_catalog.pg_get_userbyid(d.datdba) AS db_owner,
+        CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+            THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname))
+            ELSE 'No Access'
+        END AS db_size
+    FROM 
+        pg_catalog.pg_database d
+    ORDER BY 
+        db_size DESC
 ;
